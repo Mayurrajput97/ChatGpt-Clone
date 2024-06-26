cb95932823bf4a91abba8f337126ee29f1b79306
MayurRajput97 <mayurrajput7827@gmail.com>
2024-06-26 23:09:52 +0530

    Onbording page UI

--- client/lib/features/onboarding/pages/onboarding_page.dart
+++ client/lib/features/onboarding/pages/onboarding_page.dart
@@ -1,4 +1,6 @@
+import 'package:client/features/onboarding/widgets/DescriptionWidget.dart';
 import 'package:client/features/onboarding/widgets/logo_widget.dart';
+import 'package:client/features/onboarding/widgets/title_widget.dart';
 import 'package:flutter/material.dart';
 
 class OnboardingPage extends StatelessWidget {
@@ -8,12 +10,26 @@ class OnboardingPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[200],
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const [
-            LogoWidget(),
-          ],
+      body: const Center(
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              LogoWidget(),
+              SizedBox(
+                height: 20,
+              ),
+              TitleWidget(text: "Welcome To ChatGPT"),
+              SizedBox(
+                height: 15,
+              ),
+              DescriptionWidget(
+                text:
+                    "We believe our research will eventually lead to artificial general intelligence, a system that can solve human-level problems.",
+              ),
+            ],
+          ),
         ),
       ),
     );
